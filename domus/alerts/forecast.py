c2eb44888592462004f2cb711084933ba2ff9b41
Esteban Siravegna <esiravegna@gamil.com>
2015-10-05 00:15:10 -0300

    It tweets!!!!

--- domus/alerts/forecast.py
+++ domus/alerts/forecast.py
@@ -1,18 +1,58 @@
-THRESHOLDS = []
+# -*- coding: utf-8 -*-
+import datetime
+from emoji import emojize
+RAIN_THRESHOLD = {
+    u'es improbable que llueva': (0, 40),
+    emojize(u'va a llover :umbrella:'): (75, 100),
+    u'puede llover': (40, 74)
+    }
+
 
 class Forecast(object):
     """
     A simple forecast that tweets the day that will do today, tomorrow and the rest of the day
     based on the wunderground board
     """
-    def __init__(self, interval):
+    def __init__(self, data_server, twitter_client):
         """
-        :param interval:
+        :param data_server: Current influxdb server
+        :param twitter_client: Twitter interface to do da magic
         :return:
         """
-        self.interval = interval
-        pass
-    def __call__(self, data_server,twitter_client):
+        self.server = data_server
+        self.twitter = twitter_client
 
-        pass
+    def __forecast_to_message(self, threshold, when=0, measurement='rain_forecast'):
+        """
+        translates the rain forecast to actual sentences based on the RAIN_THRESHOLD constant below
+        :param threshold a dict of {'message':(upper,lower)} used to generate the message
+        :param when: 0 for today 1, for tomorrow and so on
+        :param measurement The measurement to be queried, rain_forecast as the default.
+        :return: a proper twitteable string.
+        """
+        query = "SELECT value FROM {}".format(measurement) +\
+                " WHERE board='wunderground' AND forecast_for = '{dt.year}-{dt.month}-{dt.day}' " \
+                " ORDER BY time desc LIMIT 1".format(dt=datetime.datetime.now() + datetime.timedelta(days=when))
+        message = False
+        try:
+            rs = self.server.query(query)
+            forecast = list(rs.get_points(measurement=measurement))[0]['value']
+            for key, value in threshold.iteritems():
+                if value[0] <= forecast <= value[1]:
+                    message = key
+                    break
+        except ValueError:
+            raise Exception("Unable to connect to the dataserver")
+        return message
 
+    def __call__(self):
+        result = False
+        current_time = datetime.datetime.now().hour
+        today = self.__forecast_to_message(RAIN_THRESHOLD)
+        tomorrow = self.__forecast_to_message(RAIN_THRESHOLD, when=1)
+        if today and tomorrow:
+            tweet_response = u"Hoy " if current_time <= 12 else u"Esta tarde "
+            tomorrow = "lo mismo" if today == tomorrow else tomorrow
+            tweet_response = tweet_response + u"{}. Mañana, {}".format(today,tomorrow)
+            result = self.twitter.tweet(tweet_response)
+        return result
--- utils/config.py
+++ utils/config.py
@@ -3,17 +3,17 @@ import os
 """
 Influx db
 """
-INFLUX_DB_SERVER = os.getenv("DOMUS_INFLUX_DB_SERVER", "localhost")
+INFLUX_DB_SERVER = os.getenv("DOMUS_INFLUX_DB_SERVER", "localhost").rstrip()
 INFLUX_DB_PORT = os.getenv("DOMUS_INFLUX_DB_PORT", 8086)
-INFLUX_DB_USER = os.getenv("DOMUS_INFLUX_DB_USER", "user")
-INFLUX_DB_PASSWORD = os.getenv("DOMUS_INFLUX_DB_PASSWORD", "password")
-INFLUX_DB_TABLE = os.getenv("DOMUS_INFLUX_DB_TABLE", "domus")
+INFLUX_DB_USER = os.getenv("DOMUS_INFLUX_DB_USER", "user").rstrip()
+INFLUX_DB_PASSWORD = os.getenv("DOMUS_INFLUX_DB_PASSWORD", "password").rstrip()
+INFLUX_DB_TABLE = os.getenv("DOMUS_INFLUX_DB_TABLE", "domus").rstrip()
 
 
 """
 Twitter API
 """
-TWITTER_APP_KEY = os.getenv("DOMUS_TWITTER_APP_KEY", "KEY")
-TWITTER_APP_SECRET = os.getenv("DOMUS_TWITTER_APP_SECRET", "SECRET")
-TWITTER_OAUTH_TOKEN = os.getenv("DOMUS_TWITTER_OAUTH_TOKEN", "OAUTH")
-TWITTER_OAUTH_SECRET = os.getenv("DOMUS_TWITTER_TOKEN_SECRET", "OAUT_SECRET")
+TWITTER_APP_KEY = os.getenv("DOMUS_TWITTER_APP_KEY", "KEY").rstrip()
+TWITTER_APP_SECRET = os.getenv("DOMUS_TWITTER_APP_SECRET", "SECRET").rstrip()
+TWITTER_OAUTH_TOKEN = os.getenv("DOMUS_TWITTER_OAUTH_TOKEN", "OAUTH").rstrip()
+TWITTER_OAUTH_SECRET = os.getenv("DOMUS_TWITTER_TOKEN_SECRET", "OAUT_SECRET").rstrip()
