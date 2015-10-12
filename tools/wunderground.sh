e689d7034ab511c94700975e05bfae9ea2fdbd05
Esteban Siravegna <esiravegna@gmail.com>
2015-10-12 16:24:37 -0300

    Removing unused wunderground.sh as it is now a job

--- tools/wunderground.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${BASE_DIR}/../"
-export PYTHONPATH=$PYTHONPATH:`pwd`
-source venv/bin/activate
-#environmental variables
-source venv/bin/settings
-python -m utils.wunderground
